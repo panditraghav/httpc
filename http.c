9d2bb9c1d9233971b07f7219eb803a9706318ca2
Raghav Pandit <panditraghav@gmail.com>
2025-07-06 18:04:39 +0530

    getaddrinfo test

--- http.c
+++ http.c
@@ -1,41 +1,48 @@
-#include "array.h"
-#include <stddef.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <sys/socket.h>
 
-
-int main() {
-  IntArray arr = int_array_new(2);
-  int_array_push_back(&arr, 10);
-  int_array_push_back(&arr, 20);
-  int_array_push_back(&arr, 33);
-  int_array_push_back(&arr, 22);
-  int_array_push_back(&arr, 1234);
-
-  for (size_t i = 0; i < arr.len; i++) {
-    printf("%d\n", arr.internal_array[i]);
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "usage: httpc hostname\n");
+    return 1;
   }
 
-  printf("Length: %zu\n", arr.len);
-  printf("Capacity: %zu\n", arr.capacity);
+  int gai_status;
+  struct addrinfo *info;
+  struct addrinfo hints = {0};
 
-  int popped;
-  int pop_error;
+  hints.ai_family = AF_UNSPEC; // Don't care if IPv4 or IPv6
+  // hints.ai_flags = AI_PASSIVE;     // Fill in my IP address
+  hints.ai_socktype = SOCK_STREAM; // TCP
 
-  pop_error = int_array_pop(&arr, &popped);
-  printf("popped: %d, pop_error: %d\n", popped, pop_error);
-
-  pop_error = int_array_pop(&arr, &popped);
-  printf("popped: %d, pop_error: %d\n", popped, pop_error);
-
-  pop_error = int_array_pop(&arr, &popped);
-  printf("popped: %d, pop_error: %d\n", popped, pop_error);
-
-  for (size_t i = 0; i < arr.len; i++) {
-    printf("%d\n", arr.internal_array[i]);
+  if ((gai_status = getaddrinfo(argv[1], NULL, &hints, &info)) !=
+      0) {
+    fprintf(stderr, "Some error occured: %s\n", gai_strerror(gai_status));
+    return 2;
   }
 
-  printf("Length: %zu\n", arr.len);
-  printf("Capacity: %zu\n", arr.capacity);
+  for (struct addrinfo *p = info; p != NULL; p = p->ai_next) {
+    char *family;
+    char ipstr[INET6_ADDRSTRLEN];
+    void *addr;
+
+    if (p->ai_family == AF_INET) {
+      // IPv4
+      family = "IPv4";
+      struct sockaddr_in *sin = (struct sockaddr_in *)p->ai_addr;
+      addr = &(sin->sin_addr);
+    } else {
+      struct sockaddr_in6 *sin6 = (struct sockaddr_in6 *)p->ai_addr;
+      addr = &(sin6->sin6_addr);
+      family = "IPv6";
+    }
+
+    inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+    printf("%s : %s\n", family, ipstr);
+  }
+
+  freeaddrinfo(info);
 }
